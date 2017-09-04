d5466400a25ed522c9f6b140e2670f4d39a3d838
JP Hastings-Spital <jp@deliveroo.co.uk>
2017-09-04 15:46:51 +0100

    Added Memory Caching

---
+++ lib/rspec/determinator.rb
@@ -0,0 +1,48 @@
+module RSpec
+  module Determinator
+    def self.included(by)
+      by.extend(DSL)
+
+      by.let(:fake_determinator) { FakeControl.new }
+      by.before do
+        allow(Determinator::Control).to receive(:new).and_return(fake_determinator)
+      end
+    end
+
+    module DSL
+      def forced_determination(name, result, only_for: nil)
+        before do
+          fake_determinator.mock_result(
+            name,
+            result,
+            only_for: only_for
+          )
+        end
+      end
+    end
+
+    class FakeControl
+      def initialize
+        @mocked_results = {}
+      end
+
+      def mock_result(name, result, only_for: nil)
+        @mocked_results[name.to_s][only_for || {}] = result
+      end
+
+      def fake_determinate(name, id: nil, guid: nil, constraints: {})
+        constraints[:id] = id if id
+        constraints[:guid] = guid if guid
+
+        return false unless @mocked_results[name.to_s].has_key?(constraints)
+        @mocked_results[name.to_s][constraints]
+      end
+      alias_method :feature_flag_on?, :fake_determinate
+      alias_method :which_variant, :fake_determinate
+    end
+  end
+end
+
+RSpec.configure do |conf|
+  conf.include RSpec::Determinator, :determinator_support
+end
