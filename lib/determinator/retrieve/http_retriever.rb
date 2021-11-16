754f1ed733341d74dbd230840e745ec24e081bb4
zakhar_antoshchenko <zakhar.antoschenko@deliveroo.co.uk>
2021-11-16 13:24:49 +0200

    FE-42 Add before and after hooks for http retriever

--- lib/determinator/retrieve/http_retriever.rb
+++ lib/determinator/retrieve/http_retriever.rb
@@ -10,11 +10,19 @@ module Determinator
       end
 
       def retrieve(name)
+        before_hook
         response = @connection.get("/features/#{name}")
-        return Determinator::Serializers::JSON.load(response.body) if response.status == 200
-        return MissingResponse.new if response.status == 404
+        if response.status == 200
+          after_hook(Determinator::Serializers::JSON.load(response.body))
+          return Determinator::Serializers::JSON.load(response.body)
+        end
+        if response.status == 404
+          after_hook(MissingResponse.new)
+          return MissingResponse.new
+        end
       rescue => e
         Determinator.notice_error(e)
+        after_hook(ErrorResponse.new)
         ErrorResponse.new
       end
 
@@ -28,6 +36,26 @@ module Determinator
       # @return [String, nil] a feature name or nil
       def get_name(url)
         (url.match('features\/(.*)\z') || [])[1]
+      end
+
+      def before_retrieve(&block)
+        @before_retrieve = block
+      end
+
+      def after_retrieve(&block)
+        @after_retrieve = block
+      end
+
+      private
+
+      def after_hook(args)
+        return unless @after_retrieve
+        @after_retrieve.call(args)
+      end
+
+      def before_hook
+        return unless @before_retrieve
+        @before_retrieve.call
       end
     end
   end
--- spec/determinator/retrieve/http_retreiver_spec.rb
+++ spec/determinator/retrieve/http_retreiver_spec.rb
@@ -3,27 +3,28 @@ require 'spec_helper'
 require 'webmock/rspec'
 
 RSpec.describe Determinator::Retrieve::HttpRetriever do
+  let(:client) {
+    Faraday.new(base_url)
+  }
+  let(:base_url) { 'http://actortracking.dev' }
+  let(:service_name) { 'MY-SERVICE' }
+  let(:feature_id) { 'some-feature' }
+  let(:feature_json) { {
+    name: "Feature one",
+    identifier: "feature",
+    bucket_type: "id",
+    structured_bucket: "request.customer.guid",
+    target_groups: [{
+                      rollout: 65536,
+                      constraints: {}
+                    }],
+    active: true,
+    overrides: {}
+  } }
+  let(:expected_url) { "#{base_url}/features/#{feature_id}" }
+
   describe '#retrieve' do
     subject(:retrieve) { described_class.new(params).retrieve(feature_id) }
-    let(:client){
-      Faraday.new(base_url)
-    }
-    let(:base_url) { 'http://actortracking.dev' }
-    let(:service_name) { 'MY-SERVICE' }
-    let(:feature_id) { 'some-feature' }
-    let(:feature_json) { {
-      name: "Feature one",
-      identifier: "feature",
-      bucket_type: "id",
-      structured_bucket: "request.customer.guid",
-      target_groups: [{
-        rollout: 65536,
-        constraints: {}
-      }],
-      active: true,
-      overrides: {}
-    } }
-    let(:expected_url) { "#{base_url}/features/#{feature_id}" }
 
     context 'when client is injected' do
       let(:params) { { connection: client } }
@@ -38,6 +39,69 @@ RSpec.describe Determinator::Retrieve::HttpRetriever do
         it 'raises an ArgumentError' do
           expect { retrieve }.to raise_error(ArgumentError)
         end
+      end
+    end
+  end
+
+  describe 'hooks' do
+    subject { described_class.new(params) }
+    let(:retrieve) { subject.retrieve(feature_id) }
+
+    context 'when use before hook' do
+      let(:foo_bar) { double('FooBar') }
+      let(:params) { { connection: client } }
+
+      before :each do
+        allow(foo_bar).to receive(:some_method).and_return(true)
+
+        allow(Determinator).to receive(:notice_error)
+        stub_request(:get, expected_url).
+          to_return(status: 200, body: feature_json.to_json)
+      end
+
+      it 'runs block in retrieve' do
+        expect(foo_bar).to receive(:some_method).once
+        subject.before_retrieve do
+          foo_bar.some_method
+        end
+        retrieve
+      end
+
+      it 'does not run block if retrieve is not called' do
+        expect(foo_bar).not_to receive(:some_method)
+        subject.before_retrieve do
+          foo_bar.some_method
+        end
+        subject
+      end
+    end
+
+    context 'when use after hook' do
+      let(:foo_bar) { double('FooBar') }
+      let(:params) { { connection: client } }
+
+      before :each do
+        allow(foo_bar).to receive(:some_method).and_return(true)
+
+        allow(Determinator).to receive(:notice_error)
+        stub_request(:get, expected_url).
+          to_return(status: 200, body: feature_json.to_json)
+      end
+
+      it 'runs block in retrieve' do
+        expect(foo_bar).to receive(:some_method).once
+        subject.after_retrieve do |res|
+          foo_bar.some_method
+        end
+        retrieve
+      end
+
+      it 'does not run block if retrieve is not called' do
+        expect(foo_bar).not_to receive(:some_method)
+        subject.after_retrieve do |res|
+          foo_bar.some_method
+        end
+        subject
       end
     end
   end
