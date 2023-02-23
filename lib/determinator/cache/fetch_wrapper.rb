e475b7f5689a8f9bc328d5ef3be3bf48b3dd1aa4
Orest Kulik <okulik@users.noreply.github.com>
2023-02-23 14:42:06 +0100

    Break up mutex sync scopes

--- lib/determinator/cache/fetch_wrapper.rb
+++ lib/determinator/cache/fetch_wrapper.rb
@@ -13,24 +13,24 @@ module Determinator
       # Call walks through each cache, returning a value if the item exists in
       # any cache, otherwise popularing each cache with the value of yield.
       def call(feature_name)
+        value = read_and_upfill(feature_name)
+
+        # if the value is missing and we cache it, return the missing response
+        return value if value.is_a?(MissingResponse) && @cache_missing
+
+        #otherwise only return the non nil/notice_missing_feature value
+        return value if !value.nil? && !(value.is_a?(MissingResponse) && !@cache_missing)
+
+        value_to_write = yield
+        return value_to_write if value_to_write.is_a?(ErrorResponse)
+
         @mutex.synchronize do
-          value = read_and_upfill(feature_name)
-
-          # if the value is missing and we cache it, return the missing response
-          return value if value.is_a?(MissingResponse) && @cache_missing
-
-          #otherwise only return the non nil/notice_missing_feature value
-          return value if !value.nil? && !(value.is_a?(MissingResponse) && !@cache_missing)
-
-          value_to_write = yield
-          return value_to_write if value_to_write.is_a?(ErrorResponse)
-
           @caches.each do |cache|
             cache.write(key(feature_name), value_to_write)
           end
-
-          return value_to_write
         end
+
+        return value_to_write
       end
 
       def expire(feature_name)
@@ -53,16 +53,20 @@ module Determinator
       # @param url [String] a feature name
       # @return [Feature, MissingResponse] nil when no value is found
       def read_and_upfill(feature_name)
-        @caches.each.with_index do |cache, index|
-          if cache.exist?(key(feature_name))
-            value = cache.read(key(feature_name))
-            @caches[0...index].each do |cache|
-              cache.write(key(feature_name), value)
+        @mutex.synchronize do
+          @caches.each.with_index do |cache, index|
+            if cache.exist?(key(feature_name))
+              value = cache.read(key(feature_name))
+              @caches[0...index].each do |cache|
+                cache.write(key(feature_name), value)
+              end
+
+              return value
             end
-            return value
           end
+
+          return nil
         end
-        return nil
       end
     end
   end
